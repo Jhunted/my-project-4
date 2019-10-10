5cfe247ba2b1df258dd5059ea79829e550128103
Joseph Hunt <jhunted54@gmail.com>
2019-10-09 21:58:24 -0500

    adds collection component

---
+++ src/components/Collection/Collection.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// import React from 'react';
+import './Collection.css';
+
+const Collection = (props) => {      
+
+let collection = props.collection.map((collection) => (
+    <div className="anime">
+        <div className="anime-thumbnail-container">
+        </div>
+        <div className="anime-details">
+            <h5>{collection.title}</h5>
+            <p >Description:</p>
+            <p>Title: {collection.title}</p>
+            <p>Synopsis: {collection.synopsis}</p>
+            <button onClick={(e)=>props.handleDelAnime(e, collection) }>DELETE</button>
+        </div>
+    </div>        
+))
+
+    return (
+        <div className="collection">
+                <h3>Collection</h3>
+            <section>
+                { collection }
+            </section>
+        </div>
+    )
+}
+export default Collection;
